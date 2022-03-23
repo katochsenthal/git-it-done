702d420f91980368b6e0c301d155a8d94d327939
katochsenthal <katoch.senthal@gmail.com>
2022-03-23 11:51:16 -0400

    displaying repos, open issues and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,20 +1,29 @@
-var getUserRepos = function (user) {
-  // format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-  //   make a request to the url
-  fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
-    });
-  });
-};
-
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  //   make a request to the url
+  fetch(apiUrl)
+    .then(function (response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert("Error: GitHub User Not Found");
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to GitHub");
+    });
+};
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -32,6 +41,12 @@ var displayRepos = function (repos, searchTerm) {
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
+
+  // check if api returned any repository
+  if (repos.length === 0) {
+    repoContentEl.textContent = "No repositories found.";
+    return;
+  }
 
   // loop over repos
   for (var i = 0; i < repos.length; i++) {
@@ -54,7 +69,7 @@ var displayRepos = function (repos, searchTerm) {
 
     // check if current repo has issues or not
     if (repos[i].open_issues_count > 0) {
-      statusEl.innerHtml =
+      statusEl.innerHTML =
         "<i class='fas fa-times status-icon icon-danger'></i>" +
         repos[i].open_issues_count +
         " issue(s)";
